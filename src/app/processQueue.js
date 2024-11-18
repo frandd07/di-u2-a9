c75e675276cccce10445aab3f735147b5fe9549e
frandd07 <fran.doblado.diaz@gmail.com>
2024-11-18 11:17:28 +0100

    Update ProcessQueue.js

--- src/app/processQueue.js
+++ src/app/processQueue.js
@@ -3,6 +3,13 @@ export function getFinalState(baseState, queue) {
   
     // TODO: haz algo con la cola...
   
+    for(let i = 0; i <queue.length; i++){
+      if(typeof queue[i] === 'function'){
+        finalState = queue[i](finalState)
+      }else{
+        finalState = queue[i];
+      }
+    }
     return finalState;
   }
   
